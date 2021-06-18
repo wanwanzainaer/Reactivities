e7ca4c8215590fbf6802e93d39b11dd3f63684ba
simon <wanwanzainaer@gmail.com>
2021-06-17 22:59:45 -0700

    Add ActivityDashboard and ActivityList

---
+++ client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import { Button, Container, Menu } from 'semantic-ui-react';
+
+const NavBar: FC = () => {
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item header>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: '10px' }}
+          />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="Activities" />
+        <Menu.Item>
+          <Button positive content="Create Activity" />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
+
+export default NavBar;
