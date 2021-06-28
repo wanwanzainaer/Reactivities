fbe31cf6c5ee813a3afd11eb538e55cb5bc98600
simon <wanwanzainaer@gmail.com>
2021-06-27 23:47:59 -0700

    Add edit  and key to help render

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,24 +5,36 @@ import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 import { observer } from 'mobx-react-lite';
-import { Route, Switch } from 'react-router';
+import { Route, Switch, useLocation } from 'react-router';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 const App: React.FC = () => {
+  const location = useLocation();
   return (
     <>
-      <NavBar />
-      <Container style={{ marginTop: '7em' }}>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/activities" component={ActivityDashboard} />
-          <Route path="/activities/:id" component={ActivityDetails} />
-          <Route path="/createActivity" component={ActivityForm} />
-        </Switch>
-        {/* <ActivityDashboard /> */}
-      </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={'/(.+)'}
+        render={() => (
+          <>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Switch>
+                <Route exact path="/activities" component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={['/createActivity', '/manage/:id']}
+                  component={ActivityForm}
+                />
+              </Switch>
+              {/* <ActivityDashboard /> */}
+            </Container>
+          </>
+        )}
+      />
     </>
   );
 };
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,6 @@
 import { makeAutoObservable, runInAction } from 'mobx';
 import agent from '../api/agent';
 import { Activity } from '../models/activity';
-import { v4 as uuid } from 'uuid';
 
 export default class ActivityStore {
   activities: Activity[] = [];
@@ -43,14 +42,19 @@ export default class ActivityStore {
 
   loadActivity = async (id: string) => {
     let activity = this.getActivity(id);
-    if (activity) this.selectedActivity = activity;
-    else {
+    if (activity) {
+      this.selectedActivity = activity;
+      return activity;
+    } else {
       this.loadingInitial = true;
       try {
         activity = await agent.Activities.details(id);
         this.setActivity(activity);
-        this.selectedActivity = activity;
+        runInAction(() => {
+          this.selectedActivity = activity;
+        });
         this.setLoadingInitial(false);
+        return activity;
       } catch (e) {
         console.log(e);
         this.setLoadingInitial(false);
@@ -84,7 +88,6 @@ export default class ActivityStore {
 
   createActivity = async (activity: Activity) => {
     this.loading = true;
-    activity.id = uuid();
     try {
       await agent.Activities.create(activity);
       runInAction(() => {
