2a78757a181c56f1d787e5ba2281bf436f4d1d6f
simon <wanwanzainaer@gmail.com>
2021-06-28 22:58:49 -0700

    Add some style

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -23,6 +23,19 @@ export default class ActivityStore {
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
     );
   }
+
+  get groupedActivities() {
+    return Object.entries(
+      this.activitiesByDate.reduce((activities, activity) => {
+        const date = activity.date;
+        activities[date] = activities[date]
+          ? [...activities[date], activity]
+          : [activity];
+        return activities;
+      }, {} as { [key: string]: Activity[] })
+    );
+  }
+
   loadActivities = async () => {
     this.loadingInitial = true;
     try {
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import 'react-calendar/dist/Calendar.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
 import { store, StoreContext } from './app/stores/store';
